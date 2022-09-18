23be9131f9269b01e0016ad1b23483d518f97221
Пользователь <@DESKTOP-S4IFRUA>
2022-09-18 16:17:38 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Figura.cs
@@ -0,0 +1,63 @@
+﻿
+namespace ClassLibrary1
+{
+    public class Figura
+    {
+        private Point[] points;
+        private double perimetr = 0;
+
+        public string Name { get; set; }
+        public double Perimetr => perimetr;
+        public Figura(Point p1, Point p2 , Point p3)
+        {
+            points = new Point[3];
+            points[0] = p1;
+            points[1] = p2;
+            points[2] = p3;
+
+            PerimeterCalculator();
+        }
+
+        public Figura(Point p1, Point p2, Point p3, Point p4 )
+        {
+            points = new Point[4];
+            points[0] = p1;
+            points[1] = p2;
+            points[2] = p3;
+            points[3] = p4;
+
+            PerimeterCalculator();
+        }
+
+        public Figura(Point p1, Point p2, Point p3, Point p4, Point p5)
+        {
+            points = new Point[5];
+            points[0] = p1;
+            points[1] = p2;
+            points[2] = p3;
+            points[3] = p4;
+            points[4] = p5;
+
+            PerimeterCalculator();
+        }
+         private double LengthSide(Point A, Point B)
+        {
+            double x2 = Math.Pow( Math.Abs(A.X - B.X),2) ;
+            double y2 = Math.Pow( Math.Abs(A.Y - B.Y),2) ;
+
+            return Math.Sqrt (x2 + y2);
+        }
+
+         private void PerimeterCalculator()
+        {
+            for(int i = 0; i < points.Length - 1; i++)
+            {
+                perimetr += LengthSide(points[i], points[i+1]);
+            }
+            perimetr += LengthSide(points[0], points[points.Length - 1]);
+
+        }
+
+
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿using ClassLibrary1;
+
+Point p1 = new Point (0 ,0);
+Point p2 = new Point (4 ,0);
+Point p3 = new Point(0, 4);
+Point p4 = new Point (4 ,4);
+
+Figura fig = new Figura(p1, p2, p3, p4);
+Console.WriteLine(fig.Perimetr);
+

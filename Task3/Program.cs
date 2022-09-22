b99923556c1178f4170753c328f1c15c255aa93a
steppans <91904632+steppans@users.noreply.github.com>
2022-09-22 15:42:25 +0300

    Finish

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -2,8 +2,8 @@
 
 Point p1 = new Point (0 ,0);
 Point p2 = new Point (4 ,0);
-Point p3 = new Point(4, 4);
-Point p4 = new Point (0 ,4);
+Point p3 = new Point(0, 4);
+Point p4 = new Point (4 ,4);
 
 Figura fig = new Figura(p1, p2, p3, p4);
 Console.WriteLine(fig.Perimetr);
